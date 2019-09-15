849d5292967b4604eaed4d5a6cde10399d0b0180
Sachin Goyal <sachin@fanduel.com>
2019-09-15 19:06:00 +0100

    Hook up GQL with backend services

--- src/main/java/org/baat/core/transfer/user/SignupRequest.java
+++ src/main/java/org/baat/core/transfer/user/SignupRequest.java
@@ -1,81 +1,88 @@
 package org.baat.core.transfer.user;
 
 public class SignupRequest {
-	private String email;
-	private String name;
-	private String avatarUrl;
-	private String password;
+    private String email;
+    private String name;
+    private String avatarUrl;
+    private String password;
 
-	public SignupRequest() {
-	}
+    public SignupRequest() {
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public SignupRequest(String email, String name, String avatarUrl, String password) {
+        this.email = email;
+        this.name = name;
+        this.avatarUrl = avatarUrl;
+        this.password = password;
+    }
 
-	public void setEmail(final String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setEmail(final String email) {
+        this.email = email;
+    }
 
-	public void setName(final String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getAvatarUrl() {
-		return avatarUrl;
-	}
+    public void setName(final String name) {
+        this.name = name;
+    }
 
-	public void setAvatarUrl(final String avatarUrl) {
-		this.avatarUrl = avatarUrl;
-	}
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setAvatarUrl(final String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
 
-	public void setPassword(final String password) {
-		this.password = password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	@Override
-	public boolean equals(final Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
+    public void setPassword(final String password) {
+        this.password = password;
+    }
 
-		final SignupRequest that = (SignupRequest) o;
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
-		if (email != null ? !email.equals(that.email) : that.email != null)
-			return false;
-		if (name != null ? !name.equals(that.name) : that.name != null)
-			return false;
-		if (avatarUrl != null ? !avatarUrl.equals(that.avatarUrl) : that.avatarUrl != null)
-			return false;
-		return password != null ? password.equals(that.password) : that.password == null;
-	}
+        final SignupRequest that = (SignupRequest) o;
 
-	@Override
-	public int hashCode() {
-		int result = email != null ? email.hashCode() : 0;
-		result = 31 * result + (name != null ? name.hashCode() : 0);
-		result = 31 * result + (avatarUrl != null ? avatarUrl.hashCode() : 0);
-		result = 31 * result + (password != null ? password.hashCode() : 0);
-		return result;
-	}
+        if (email != null ? !email.equals(that.email) : that.email != null)
+            return false;
+        if (name != null ? !name.equals(that.name) : that.name != null)
+            return false;
+        if (avatarUrl != null ? !avatarUrl.equals(that.avatarUrl) : that.avatarUrl != null)
+            return false;
+        return password != null ? password.equals(that.password) : that.password == null;
+    }
 
-	@Override
-	public String toString() {
-		final StringBuilder sb = new StringBuilder("SignupRequest{");
-		sb.append("email='").append(email).append('\'');
-		sb.append(", name='").append(name).append('\'');
-		sb.append(", avatarUrl='").append(avatarUrl).append('\'');
-		sb.append(", password='").append(password).append('\'');
-		sb.append('}');
-		return sb.toString();
-	}
+    @Override
+    public int hashCode() {
+        int result = email != null ? email.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (avatarUrl != null ? avatarUrl.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("SignupRequest{");
+        sb.append("email='").append(email).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", avatarUrl='").append(avatarUrl).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
