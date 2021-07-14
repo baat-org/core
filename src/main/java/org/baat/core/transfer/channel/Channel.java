335984a01171ac12b44d51e6f678c93db8258942
Sachin Goyal <sachin@fanduel.com>
2021-07-14 22:49:34 +0100

    Working Controller

---
+++ src/main/java/org/baat/core/transfer/channel/Channel.java
@@ -0,0 +1,61 @@
+package org.baat.core.transfer.channel;
+
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.util.Objects;
+
+public class Channel {
+
+    @Positive
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    public Channel() {
+    }
+
+    public Channel(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Channel channel = (Channel) o;
+        return Objects.equals(id, channel.id) && Objects.equals(name, channel.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Channel{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
